506bf14a2592c54e7b0bcb3cd5845215a5f76ac4
priyasharanam1 <p1910413219@gmail.com>
2024-09-08 19:46:06 +0530

    login page call

--- blog/src/app/login/page.jsx
+++ blog/src/app/login/page.jsx
@@ -1,4 +1,3 @@
-// pages/login.js
 'use client';
 import { useState } from 'react';
 import axios from 'axios';
@@ -6,15 +5,15 @@ import styles from './loginPage.module.css';
 
 export default function LoginPage() {
   const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
   const [error, setError] = useState(null);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+    setError(null); // Clear the previous error before making the new request
 
     try {
-      const response = await axios.post('http://localhost:8000/api/v1/users/login', { email, username, password });
+      const response = await axios.post('http://localhost:8000/api/v1/users/login', { email, password });
 
       if (response.status === 200) {
         console.log('success login');
@@ -27,40 +26,33 @@ export default function LoginPage() {
   return (
     <div className={styles.container}>
       <div className={styles.wrapper}>
-        <h1>Login</h1>
-        {error && <p style={{ color: 'red' }}>{error}</p>}
-        <form onSubmit={handleSubmit}>
+        <h1 className={styles.heading}>Login</h1>
+        {error && <p className={styles.error}>{error}</p>}
+        <form className={styles.loginForm} onSubmit={handleSubmit}>
           <div>
-            <label htmlFor="email">Email</label>
-            <input
+            <label className={styles.label} htmlFor="email">Email</label>
+            <input className={styles.input}
               type="email"
               id="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               placeholder="Enter email"
+              required
             />
           </div>
           <div>
-            <label htmlFor="username">Username</label>
+            <label className={styles.label} htmlFor="password">Password</label>
             <input
-              type="text"
-              id="username"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
-              placeholder="Enter username"
-            />
-          </div>
-          <div>
-            <label htmlFor="password">Password</label>
-            <input
+              className={styles.input}
               type="password"
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Enter password"
+              required
             />
           </div>
-          <button type="submit">Login</button>
+          <button className={styles.btn} type="submit">Login</button>
         </form>
       </div>
     </div>
